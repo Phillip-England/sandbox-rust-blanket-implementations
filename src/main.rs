698a93ca52856c48f3e22c54482673e617e90c83
Phillip England <phillip.mark.england@gmail.com>
2024-12-21 08:53:21 -0600

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let some_str = "i am going to shout";
+    let shout = some_str.shout();
+    let some_string = String::from("i also shout");
+    let shout2 = some_string.shout();
+    println!("{} - {}", shout, shout2);
+}
+
+// we deine a trait we will use to "extend" the &str and String types
+pub trait SomeExtension {
+    fn shout(&self) -> String;
+
+}
+
+
+// here, take our extension and implement it on any type which can be references as a str
+impl<T> SomeExtension for T
+where
+    T: AsRef<str>,
+{
+    fn shout(&self) -> String {
+        let s = self.as_ref();
+        format!("{}!", s.to_uppercase())
+    }
 }
